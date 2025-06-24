d9f9a56624e6d8ffcffaa7b9c872ae7d951ea7a6
MugiSus <mmgiss256@gmail.com>
2025-06-25 01:16:57 +0900

    adjust style

--- src/App.tsx
+++ src/App.tsx
@@ -12,24 +12,24 @@ function App() {
   }
 
   return (
-    <main class="m-0 flex flex-col justify-center text-center font-sans text-base leading-6 font-normal text-neutral-900 dark:text-neutral-100 dark:bg-neutral-800">
-      <h1 class="text-center">Welcome to Tauri + Solid</h1>
+    <main class="flex flex-col gap-4 justify-center text-center font-sans text-base leading-6 font-normal text-neutral-900 dark:text-neutral-100 dark:bg-neutral-800">
+      <h1 class="text-center font-bold text-2xl">Welcome to Tauri + Solid</h1>
 
       <div class="flex justify-center">
         <a href="https://vitejs.dev" target="_blank" class="font-medium text-blue-500 no-underline hover:text-blue-600 dark:hover:text-cyan-400">
-          <img src="/vite.svg" class="h-24 p-6 transition-[filter] duration-700 hover:drop-shadow-[0_0_2em_#747bff]" alt="Vite logo" />
+          <img src="/vite.svg" class="h-32 p-6 transition-[filter] duration-700 hover:drop-shadow-[0_0_2rem_#747bff]" alt="Vite logo" />
         </a>
         <a href="https://tauri.app" target="_blank" class="font-medium text-blue-500 no-underline hover:text-blue-600 dark:hover:text-cyan-400">
-          <img src="/tauri.svg" class="h-24 p-6 transition-[filter] duration-700 hover:drop-shadow-[0_0_2em_#24c8db]" alt="Tauri logo" />
+          <img src="/tauri.svg" class="h-32 p-6 transition-[filter] duration-700 hover:drop-shadow-[0_0_2rem_#24c8db]" alt="Tauri logo" />
         </a>
         <a href="https://solidjs.com" target="_blank" class="font-medium text-blue-500 no-underline hover:text-blue-600 dark:hover:text-cyan-400">
-          <img src={logo} class="h-24 p-6 transition-[filter] duration-700 hover:drop-shadow-[0_0_2em_#2f5d90]" alt="Solid logo" />
+          <img src={logo} class="h-32 p-6 transition-[filter] duration-700 hover:drop-shadow-[0_0_2rem_#2f5d90]" alt="Solid logo" />
         </a>
       </div>
       <p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
 
       <form
-        class="flex justify-center"
+        class="flex justify-center gap-1"
         onSubmit={(e) => {
           e.preventDefault();
           greet();
@@ -37,7 +37,7 @@ function App() {
       >
         <input
           id="greet-input"
-          class="mr-1 rounded-lg border border-transparent px-5 py-2.5 text-base font-medium font-sans text-neutral-900 bg-white transition-colors duration-200 shadow-[0_2px_2px_rgba(0,0,0,0.2)] outline-none dark:text-white dark:bg-neutral-900/60"
+          class="mr-1 rounded-lg border border-transparent px-5 py-2.5 text-base font-medium font-sans text-neutral-900 bg-white transition-colors duration-200 outline-none dark:text-white dark:bg-neutral-900/60"
           onChange={(e) => setName(e.currentTarget.value)}
           placeholder="Enter a name..."
         />
