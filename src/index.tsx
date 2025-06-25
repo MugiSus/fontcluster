f6733bddb1cbbe4d26f10e1c6e3db6511d72569e
MugiSus <mmgiss256@gmail.com>
2025-06-25 17:55:44 +0900

    fix: update import statement for App component to remove file extension

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
 
+import App from './App';
 import './index.css';
-import App from './App.tsx';
 
 render(() => <App />, document.querySelector('#root') as HTMLElement);
