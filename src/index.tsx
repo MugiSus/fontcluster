51e32fa537d5ae5c7990c12d2d2ff7028429e579
MugiSus <mmgiss256@gmail.com>
2025-06-30 17:14:48 +0900

    fix: correct import path for App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
 
-import App from './app';
+import App from './App';
 import './index.css';
 
 render(() => <App />, document.querySelector('#root') as HTMLElement);
