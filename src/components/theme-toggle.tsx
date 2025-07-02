6596b5f23a444d3f1a5a64e68bbee4fbb28a92f1
MugiSus <mmgiss256@gmail.com>
2025-07-02 22:38:37 +0900

    feat: add lucide-solid icons for theme toggle component

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,5 +1,6 @@
 import { useColorMode } from '@kobalte/core';
 
+import { Laptop, Sun, Moon } from 'lucide-solid';
 import { Button } from './ui/button';
 import {
   DropdownMenu,
@@ -19,21 +20,21 @@ export function ModeToggle() {
         size='sm'
         class='w-9 px-0'
       >
-        <IconSun class='size-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-        <IconMoon class='absolute size-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+        <Sun class='size-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+        <Moon class='absolute size-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
         <span class='sr-only'>Toggle theme</span>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuItem onSelect={() => setColorMode('light')}>
-          <IconSun class='mr-2 size-4' />
+          <Sun class='mr-2 size-4' />
           <span>Light</span>
         </DropdownMenuItem>
         <DropdownMenuItem onSelect={() => setColorMode('dark')}>
-          <IconMoon class='mr-2 size-4' />
+          <Moon class='mr-2 size-4' />
           <span>Dark</span>
         </DropdownMenuItem>
         <DropdownMenuItem onSelect={() => setColorMode('system')}>
-          <IconLaptop class='mr-2 size-4' />
+          <Laptop class='mr-2 size-4' />
           <span>System</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
