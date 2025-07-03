483f2b11dea8da41f5b565372c6c035caee696eb
MugiSus <mmgiss256@gmail.com>
2025-07-03 13:07:28 +0900

    fix: update titlebar layout and window properties for improved usability

--- src/components/titlebar.tsx
+++ src/components/titlebar.tsx
@@ -4,9 +4,11 @@ export function Titlebar() {
   return (
     <header
       data-tauri-drag-region
-      class='flex w-full select-none items-center justify-center p-2 text-xs tracking-widest'
+      class='flex h-8 w-full select-none items-center justify-center border-b'
     >
-      <h1 data-tauri-drag-region>FontCluster</h1>
+      <h1 data-tauri-drag-region class='mt-0.5 text-xs tracking-widest'>
+        FontCluster
+      </h1>
       <ModeToggle class='fixed right-1 top-1 mr-px' />
     </header>
   );
