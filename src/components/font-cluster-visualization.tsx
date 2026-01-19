84fe7e4f28acb3e108b1d0769a537e4c971b8549
MugiSus <mmgiss256@gmail.com>
2026-01-19 19:08:18 +0900

    Remove special visibility handling for selected fonts in the visualization.

--- src/components/font-cluster-visualization.tsx
+++ src/components/font-cluster-visualization.tsx
@@ -302,9 +302,7 @@ export function FontClusterVisualization() {
     const maxVisibleY = vb.y + vb.height / 2 + visibleHeight / 2 + padding;
 
     const filteredKeys = appState.fonts.filteredKeys;
-    const selectedFontKey = appState.ui.selectedFontKey;
     const activeWeights = new Set(visualizerWeights());
-
     const visibleFilteredPoints = [];
     const visibleUnfilteredPoints = [];
 
@@ -318,9 +316,7 @@ export function FontClusterVisualization() {
         point.y >= minVisibleY &&
         point.y <= maxVisibleY;
 
-      const isSelected = point.key === selectedFontKey;
-
-      if (isWeightIncluded && (isVisible || isSelected)) {
+      if (isWeightIncluded && isVisible) {
         if (filteredKeys.has(point.key)) {
           visibleFilteredPoints.push(point);
         } else {
