bb33647cb17ee64a3bc78ca567722e46f77728ba
MugiSus <mmgiss256@gmail.com>
2026-06-03 04:25:10 +0900

    feat: simplify LassoClearButton by removing tooltip and directly using button

--- src/components/graph/lasso-clear-button.tsx
+++ src/components/graph/lasso-clear-button.tsx
@@ -1,6 +1,5 @@
 import { LassoSelectIcon, XIcon } from 'lucide-solid';
 import { Button } from '../ui/button';
-import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip';
 
 interface LassoClearButtonProps {
   onClear: () => void;
@@ -9,20 +8,16 @@ interface LassoClearButtonProps {
 export function LassoClearButton(props: LassoClearButtonProps) {
   return (
     <div class='flex flex-col overflow-hidden rounded-full border bg-background text-muted-foreground'>
-      <Tooltip placement='top'>
-        <TooltipTrigger
-          as={Button<'button'>}
-          variant='ghost'
-          size='sm'
-          class='group relative flex h-7 rounded-none pl-2 pr-2.5'
-          onClick={() => props.onClear()}
-        >
-          <XIcon class='size-4' />
-          Clear Lasso
-          <LassoSelectIcon class='size-4' />
-        </TooltipTrigger>
-        <TooltipContent>Clear lasso selection</TooltipContent>
-      </Tooltip>
+      <Button
+        variant='ghost'
+        size='sm'
+        class='group relative flex h-7 gap-1 rounded-none pl-2 pr-2.5'
+        onClick={() => props.onClear()}
+      >
+        <XIcon class='size-4' />
+        Lasso
+        <LassoSelectIcon class='size-4' />
+      </Button>
     </div>
   );
 }
